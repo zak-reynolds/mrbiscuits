75b58ba92627b55b3d8e17077aba74192c466388
Zak Reynolds <zakary.reynolds@gmail.com>
2018-04-20 23:41:05 -0500

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public Vector3 currentVelocity = Vector3.zero;
+    public float dampTime = 0.5f;
+    public float maxSpeed = 4f;
+    	
+	void LateUpdate () {
+		if (target != null)
+        {
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                target.position + offset,
+                ref currentVelocity,
+                dampTime,
+                maxSpeed,
+                Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 6;
+
+    private CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+	void FixedUpdate () {
+        var velocity = Vector3.down;
+        velocity.x += Input.GetAxis("Horizontal");
+        velocity.z += Input.GetAxis("Vertical");
+        cc.Move(velocity.normalized * Time.deltaTime * speed);
+    }
+}
